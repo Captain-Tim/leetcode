607c4d3732df8ad0f068c26561632e92005a3a33
Tim <r06942096@ntu.edu.tw>
2024-06-05 15:15:29 +0800

    Create Find Common Characters.cpp

---
+++ 1002. Find Common Characters/Find Common Characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        const int LETTER_NUM = 26;
+        vector<int> common_char_num(LETTER_NUM, INT_MAX);
+        for (string& s : words)
+        {
+            vector<int> letters(LETTER_NUM, 0);
+            for (char c : s)
+                ++letters[c - 'a'];
+            for (int i = 0; i < LETTER_NUM; ++i)
+                common_char_num[i] = min(common_char_num[i], letters[i]);
+        }
+        vector<string> res;
+        for (int i = 0; i < LETTER_NUM; ++i)
+        {
+            while (common_char_num[i])
+            {
+                res.push_back(string(1, i + 'a'));
+                --common_char_num[i];
+            }
+        }
+        return res;
+    }
+};
