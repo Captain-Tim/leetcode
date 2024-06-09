d5183f3ef005f02888d203bdfae4c6c85a60f765
lifeisNPhard <r06942096@ntu.edu.tw>
2024-06-09 16:55:42 +0800

    Create Subarray Sums Divisible by K.cpp

---
+++ 0974. Subarray Sums Divisible by K/Subarray Sums Divisible by K.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> prefix_sum;
+        prefix_sum[0] = 1;
+        int sum = 0;
+        int res = 0;
+        for (int i : nums)
+        {
+            sum += (i % k);
+            if (sum < 0)
+                sum += k;
+            else
+                sum %= k;
+            res += prefix_sum[sum];
+            ++prefix_sum[sum];
+        }
+        return res;
+    }
+};
