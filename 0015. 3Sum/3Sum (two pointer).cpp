9ae7d68589fd48441636a1462fc2b2ff00b6e162
Tim Hou <pyfanfan40@gmail.com>
2024-09-03 16:54:46 -0400

    update code (formatting)

--- 0015. 3Sum/3Sum (two pointer).cpp
+++ 0015. 3Sum/3Sum (two pointer).cpp
@@ -22,7 +22,7 @@ public:
                         res.push_back({nums[i], nums[left], nums[right]});
                         while (left < right && nums[left] == nums[left + 1])
                             ++left;
-                        while (left <right && nums[right - 1] == nums[right])
+                        while (left < right && nums[right - 1] == nums[right])
                             --right;
                         ++left;
                         --right;
