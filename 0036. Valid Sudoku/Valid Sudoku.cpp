b6a1def2441875823837f62dac9517943a096a5f
Tim Hou <pyfanfan40@gmail.com>
2024-09-07 15:35:18 -0400

    simplified the code

--- 0036. Valid Sudoku/Valid Sudoku.cpp
+++ 0036. Valid Sudoku/Valid Sudoku.cpp
@@ -1,70 +1,33 @@
 class Solution {
-private:
-    const int ROW = 9;
-    const int COL = 9;
-    const int ROW_PER_BOX = 3;
-    const int COL_PER_BOX = 3;
-    const char EMPTY = '.';
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for (int r = 0; r < ROW; ++r)
-            if (!checkRowise(board, r))
-                return false;
-        for (int c = 0; c < COL; ++c)
-            if (!checkColumnwise(board, c))
-                return false;
-        for (int r = 0; r < ROW_PER_BOX; ++r)
-            for (int c = 0; c < COL_PER_BOX; ++c)
-                if (!checkSubboxes(board, r, c))
-                    return false;
-        return true;
-    }
-    bool checkRowise(vector<vector<char>>& board, int row)
-    {
-        vector<bool> occurred(COL + 1, false);
-        for (int c = 0; c < COL; ++c)
+        const int SIZE = 9;
+        vector<unordered_set<char>> rows(SIZE);
+        vector<unordered_set<char>> cols(SIZE);
+        vector<unordered_set<char>> boxes(SIZE);
+        for (int r = 0; r < SIZE; ++r)
         {
-            if (board[row][c] != EMPTY)
+            for (int c = 0; c < SIZE; ++c)
             {
-                int num = board[row][c] - '0';
-                if (occurred[num])
-                    return false;
-                occurred[num] = true;
-            }
-        }
-        return true;
-    }
-    bool checkColumnwise(vector<vector<char>>& board, int col)
-    {
-        vector<bool> occurred(ROW + 1, false);
-        for (int r = 0; r < ROW; ++r)
-        {
-            if (board[r][col] != EMPTY)
-            {
-                int num = board[r][col] - '0';
-                if (occurred[num])
-                    return false;
-                occurred[num] = true;
-            }
-        }
-        return true;
-    }
-    bool checkSubboxes(vector<vector<char>>& board, int box_row, int box_col)
-    {
-        vector<bool> occurred(ROW + 1, false);
-        for (int r = box_row * ROW_PER_BOX, row_boundary = (box_row + 1) * ROW_PER_BOX; r < row_boundary; ++r)
-        {
-            for (int c = box_col * COL_PER_BOX, col_boundary = (box_col + 1) * COL_PER_BOX; c < col_boundary; ++c)
-            {
-                if (board[r][c] != EMPTY)
+                char num = board[r][c];
+                if (num != '.')
                 {
-                    int num = board[r][c] - '0';
-                    if (occurred[num])
+                    if (rows[r].count(num))
                         return false;
-                    occurred[num] = true;
+                    rows[r].insert(num);
+                    
+                    if (cols[c].count(num))
+                        return false;
+                    cols[c].insert(num);
+                    
+                    int box_idx = (r / 3) * 3 + c / 3;
+                    if (boxes[box_idx].count(num))
+                        return false;
+                    boxes[box_idx].insert(num);
                 }
             }
         }
         return true;
+        
     }
 };
