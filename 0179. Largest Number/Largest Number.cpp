8ed9e0d09cef6980e767ea58542995ffdd553af6
Tim Hou <pyfanfan40@gmail.com>
2024-09-18 19:34:31 -0400

    Optimize the code

--- 0179. Largest Number/Largest Number.cpp
+++ 0179. Largest Number/Largest Number.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        auto cmp = [](int a, int b){
-            string a_str = to_string(a);
-            string b_str = to_string(b);
-            string a_cat = a_str + b_str;
-            string b_cat = b_str + a_str;
-            int idx = 0;
-            while (idx < a_cat.size())
-            {
-                if (a_cat[idx] < b_cat[idx])
-                    return false;
-                else if (a_cat[idx] > b_cat[idx])
-                    return true;
-                ++idx;
-            }
-            return false;
+        auto cmp = [](string& a, string& b){
+            return a + b > b + a;
         };
-        sort(nums.begin(), nums.end(), cmp);
+        vector<string> vec;
+        for (int i : nums)
+            vec.push_back(to_string(i));
+        sort(vec.begin(), vec.end(), cmp);
         string res;
-        for (const int i : nums)
-            res += to_string(i);
-        
-        if (res[0] == '0') 
+        for (string& s : vec)
+            res += s;
+        if (res[0] == '0')
             return "0";
         return res;
     }
