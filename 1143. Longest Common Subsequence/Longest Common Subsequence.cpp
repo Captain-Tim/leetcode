2e02de74bc0675919afce400b201aaa11abcd3f3
Tim Hou <pyfanfan40@gmail.com>
2024-09-18 00:55:40 -0400

    Optimize the space complexity

--- 1143. Longest Common Subsequence/Longest Common Subsequence.cpp
+++ 1143. Longest Common Subsequence/Longest Common Subsequence.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        const int size1 = text1.size();
-        const int size2 = text2.size();
+        int size1 = text1.size();
+        int size2 = text2.size();
         if (size1 < size2)
             return longestCommonSubsequence(text2, text1);
         vector<int> prev(size2 + 1, 0);
+        vector<int> cur(size2 + 1, 0);
         for (int i = 0; i < size1; ++i)
         {
-            vector<int> cur(size2 + 1, 0);
             for (int j = 0; j < size2; ++j)
-                cur[j + 1] = (text1[i] == text2[j]) ? prev[j] + 1 : max(cur[j], prev[j + 1]);
+                cur[j + 1] = (text1[i] == text2[j]) ? prev[j] + 1 : max(prev[j + 1], cur[j]);
             prev.swap(cur);
         }
+        
         return prev.back();
     }
 };
