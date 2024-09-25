af2f6a82f6d5fd4670a282e6764aaea438ef3768
Tim Hou <pyfanfan40@gmail.com>
2024-09-25 19:07:36 -0400

    optimization

--- 0057. Insert Interval/Insert Interval.cpp
+++ 0057. Insert Interval/Insert Interval.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if (intervals.empty())
-            return {newInterval};
         vector<vector<int>> res;
-        int cur_idx = 0;
-
-        while (cur_idx < intervals.size() && intervals[cur_idx][1] < newInterval[0])
+        int idx = 0;
+        while (idx < intervals.size() && intervals[idx][1] < newInterval[0])
+            res.push_back(intervals[idx++]);
+        while (idx < intervals.size() && newInterval[1] >= intervals[idx][0])
         {
-            res.push_back(intervals[cur_idx]);
-            ++cur_idx;
-        }
-        
-        while (cur_idx < intervals.size() && newInterval[1] >= intervals[cur_idx][0])
-        {
-            newInterval[0] = min(newInterval[0], intervals[cur_idx][0]);
-            newInterval[1] = max(newInterval[1], intervals[cur_idx][1]);
-            ++cur_idx;
+            newInterval[0] = min(newInterval[0], intervals[idx][0]);
+            newInterval[1] = max(newInterval[1], intervals[idx][1]);
+            ++idx;
         }
         res.push_back(newInterval);
-        
-        while (cur_idx < intervals.size())
-            res.push_back(intervals[cur_idx++]);
+        res.insert(res.end(), intervals.begin() + idx, intervals.end());
         return res;
     }
 };
