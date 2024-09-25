aecb8ca455fe598b33e8c7f180773f4239727f13
Tim Hou <pyfanfan40@gmail.com>
2024-09-25 01:22:20 -0400

    optimize the code

--- 0621. Task Scheduler/Task Scheduler.cpp
+++ 0621. Task Scheduler/Task Scheduler.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        const int ALPHABET_NUM = 26;
-        vector<int> freq(ALPHABET_NUM, 0);
+        const int LETTER_NUM = 26;
+        vector<int> freq(LETTER_NUM, 0);
         for (char c : tasks)
             ++freq[c - 'A'];
         sort(freq.begin(), freq.end(), greater<int>());
         int max_freq = freq[0];
-        int max_freq_num = 0;
-        for (int i : freq)
-        {
-            if (i == max_freq)
-                ++max_freq_num;
-            else
-                break;
-        }
-        int remainding_task = tasks.size() - max_freq * max_freq_num;
-        int part_num = max_freq - 1;
-        int empty_slot = part_num * (n + 1 - max_freq_num);
-        int idle_num = max(0, empty_slot - remainding_task);
-        return tasks.size() + idle_num;
+        int num_max_freq = 0;
+        for (int i = 0; i < freq.size() && freq[i] == max_freq; ++i)
+            ++num_max_freq;
+        int other_tasks = tasks.size() - num_max_freq * max_freq;
+        int empty_slot = (n + 1 - num_max_freq) * (max_freq - 1);
+        int idle_slot = max(0, empty_slot - other_tasks);
+        return tasks.size() + idle_slot;
     }
 };
