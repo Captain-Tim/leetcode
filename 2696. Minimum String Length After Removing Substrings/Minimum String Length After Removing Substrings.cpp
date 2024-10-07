92dca5ec688766ca699b2453eb4368eac5f8c5d9
Tim Hou <pyfanfan40@gmail.com>
2024-10-07 02:14:51 -0400

    add Minimum String Length After Removing Substrings.cpp

---
+++ 2696. Minimum String Length After Removing Substrings/Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<int> stk;
+        for (char c : s)
+        {
+            if (stk.empty())
+                stk.push(c);
+            else
+            {
+                if (c == 'B' && stk.top() == 'A')
+                    stk.pop();
+                else if (c == 'D' && stk.top() == 'C')
+                    stk.pop();
+                else
+                    stk.push(c);
+            }
+        }
+        return stk.size();
+    }
+};
