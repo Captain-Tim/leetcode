29643bb96f1e60df9c238285cf165d815dcb94a4
Captain-Tim <62422680+Captain-Tim@users.noreply.github.com>
2025-06-17 14:15:07 +0800

    Update Divide Intervals Into Minimum Number of Groups.cpp
    make it easier and cleaner

--- 2406. Divide Intervals Into Minimum Number of Groups/Divide Intervals Into Minimum Number of Groups.cpp
+++ 2406. Divide Intervals Into Minimum Number of Groups/Divide Intervals Into Minimum Number of Groups.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        vector<int> left(intervals.size());
-        vector<int> right(intervals.size());
-        for (int i = 0; i < intervals.size(); ++i)
-        {
-            left[i] = intervals[i][0];
-            right[i] = intervals[i][1];
-        }
-        sort(left.begin(), left.end());
-        sort(right.begin(), right.end());
-        int res = 0;
-        int cur = 0;
-        for (int left_idx = 0, right_idx = 0; left_idx < intervals.size(); )
-        {
-            if (left[left_idx] <= right[right_idx])
-            {
-                ++cur;
-                res = max(res, cur);
-                ++left_idx;
-            }
-            else
-            {
-                --cur;
-                ++right_idx;
-            }
-        }
-        return res;
+        const int IS_START_TIME = 1;
+		const int IS_END_TIME = -1;
+		vector<vector<int>> time;
+		
+		for (vector<int>& v : intervals)
+		{
+			time.push_back({v[0], IS_START_TIME});
+			time.push_back({v[1] + 1, IS_END_TIME});
+		}
+		sort(time.begin(), time.end());
+		int res = 0;
+		int cur_overlap_num = 0;
+		for (vector<int>& v : time)
+		{
+			cur_overlap_num += v[1];
+			res = max(res, cur_overlap_num);
+		}
+		return res;
     }
 };
