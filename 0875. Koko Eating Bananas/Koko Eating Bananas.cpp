a290b079ed8e0cd4a250693b8cb6b85bfab0c399
Captain-Tim <62422680+Captain-Tim@users.noreply.github.com>
2025-06-17 14:47:21 +0800

    Update Koko Eating Bananas.cpp

--- 0875. Koko Eating Bananas/Koko Eating Bananas.cpp
+++ 0875. Koko Eating Bananas/Koko Eating Bananas.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int right = 0;
-		for (int i : piles)
-			right = max(right, i);
-		
-		int left = 1;
-		while (left < right)
-		{
-			int mid = left + (right - left) / 2;
-			int time = 0;
-			for (int i : piles)
-				time += ceil( (double) i / mid);
-			if (time > h)
-				left = mid + 1;
-			else
-				right = mid;
-		}
-		return left;
+        for (int i : piles)
+            right = max(right, i);
+        
+        int left = 1;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            int time = 0;
+            for (int i : piles)
+                time += ceil( (double) i / mid);
+            if (time > h)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
     }
 };
