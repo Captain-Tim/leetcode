f6f8314c94d8cf464957af62d3a50d8e0baaae60
Captain-Tim <62422680+Captain-Tim@users.noreply.github.com>
2025-06-18 10:16:18 +0800

    Update Missing Number.cpp

--- 0268. Missing Number/Missing Number.cpp
+++ 0268. Missing Number/Missing Number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        vector<int> res(n + 1, 0);
-        for (int i = 1; i <= n; ++i)
-            res[i] = res[i & (i - 1)] + 1;
+    int missingNumber(vector<int>& nums) {
+        int res = nums.size();
+        for (int i = 0; i < nums.size(); ++i)
+            res += i - nums[i];
         return res;
     }
 };
