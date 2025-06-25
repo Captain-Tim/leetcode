ad4ce25ac43413497e71c632c51c280671589069
Captain-Tim <62422680+Captain-Tim@users.noreply.github.com>
2025-06-25 13:49:05 +0800

    Update Single Number.cpp

--- 0136. Single Number/Single Number.cpp
+++ 0136. Single Number/Single Number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int cur_sum = 0;
-        for (const int& i : nums)
+        for (int i : nums)
             cur_sum ^= i;
         return cur_sum;
     }
