eb51c59fb5801759a996cf807e1a3a0477dd8c95
Captain-Tim <aboutim2020@gmail.com>
2025-08-19 10:59:28 +0800

    update solution

--- 0739. Daily Temperatures/Daily Temperatures (optimal solution).cpp
+++ 0739. Daily Temperatures/Daily Temperatures (optimal solution).cpp
@@ -4,12 +4,11 @@ public:
         vector<int> res(temperatures.size(), 0);
         for (int i = temperatures.size() - 2; i >= 0; --i)
         {
-            int cur_temp = temperatures[i];
-            int days = i + 1;
-            
+            int days = i + 1;            
             while (temperatures[i] >= temperatures[days] && res[days] != 0)
                 days += res[days];
-            res[i] = (temperatures[i] >= temperatures[days]) ? 0 : days - i;
+            if (temperatures[i] < temperatures[days])
+                res[i] = days - i;
         }
         return res;
     }
