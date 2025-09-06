961eb584080f3692d32372a928f7b7d402e34242
Captain-Tim <62422680+Captain-Tim@users.noreply.github.com>
2025-09-06 19:15:26 +0800

    Update Valid Sudoku.cpp

--- 0036. Valid Sudoku/Valid Sudoku.cpp
+++ 0036. Valid Sudoku/Valid Sudoku.cpp
@@ -10,17 +10,19 @@ public:
         {
             for (int c = 0; c < SIZE; ++c)
             {
-                if (board[r][c] == '.')
+                if (board[r][c] == DOT)
                     continue;
-                char cur_num = board[r][c];
+                char cur_char = board[r][c];
                 int blk_idx = (r / 3) * 3 + (c / 3);
-                if (rows[r].count(cur_num) || cols[c].count(cur_num) || blks[blk_idx].count(cur_num)) 
+                if (rows[r].count(cur_char) || cols[c].count(cur_char) || blks[blk_idx].count(cur_char)) 
                     return false;
-                rows[r].insert(cur_num);
-                cols[c].insert(cur_num);
-                blks[blk_idx].insert(cur_num);
+                rows[r].insert(cur_char);
+                cols[c].insert(cur_char);
+                blks[blk_idx].insert(cur_char);
             }
         }
         return true;
     }
+private:
+    const char DOT = '.';
 };
