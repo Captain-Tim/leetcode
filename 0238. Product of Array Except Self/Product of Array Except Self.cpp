d9f719f357e24d3e5f106bc466b8b9a247b5fb08
Captain-Tim <aboutim2020@gmail.com>
2025-09-09 10:37:50 +0800

    Update Product of Array Except Self.cpp

--- 0238. Product of Array Except Self/Product of Array Except Self.cpp
+++ 0238. Product of Array Except Self/Product of Array Except Self.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> res(nums.size(), 1);
-        for (int i = 1; i < nums.size(); ++i)
-            res[i] = res[i - 1] * nums[i - 1];
-        for (int i = nums.size() - 2, product = 1; i >= 0; --i)
+        vector<int> res(nums.size(), 0);
+        for (int i = res.size() - 1, prod = 1; i >= 0; --i)
         {
-            product *= nums[i + 1];
-            res[i] *= product;
+            res[i] = prod;
+            prod *= nums[i];
+        }
+        for (int i = 0, prod = 1; i < res.size(); ++i)
+        {
+            res[i] *= prod;
+            prod *= nums[i];
         }
         return res;
     }
