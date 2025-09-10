48e312cf8568428136a8b795780fc4fe0faecf0b
Captain-Tim <aboutim2020@gmail.com>
2025-09-10 10:52:57 +0800

    Update 3Sum.cpp

--- 0015. 3Sum/3Sum (two pointer).cpp
+++ 0015. 3Sum/3Sum (two pointer).cpp
@@ -3,32 +3,29 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> res;
-        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
+        for (int cur = 0; cur < nums.size() && nums[cur] <= 0; )
         {
-            if (!i || (nums[i] != nums[i - 1]))
+            int left = cur + 1;
+            int right = nums.size() - 1;
+            int target = -nums[cur];
+            while (left < right)
             {
-                int left = i + 1;
-                int right = nums.size() - 1;
-                int target = -nums[i];
-                while (left < right)
+                int sum = nums[left] + nums[right];
+                if (sum < target)
+                    ++left;
+                else if (sum > target)
+                    --right;
+                else
                 {
-                    int sum = nums[left] + nums[right];
-                    if (sum > target)
-                        --right;
-                    else if (sum < target)
+                    res.push_back({nums[cur], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1])
                         ++left;
-                    else
-                    {
-                        res.push_back({nums[i], nums[left], nums[right]});
-                        while (left < right && nums[left] == nums[left + 1])
-                            ++left;
-                        while (left < right && nums[right - 1] == nums[right])
-                            --right;
-                        ++left;
-                        --right;
-                    }
+                    ++left;
                 }
             }
+            while (cur + 1 < nums.size() && nums[cur] == nums[cur + 1])
+                ++cur;
+            ++cur;
         }
         return res;
     }
