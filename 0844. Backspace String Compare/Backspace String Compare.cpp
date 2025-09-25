9024e0fe63c0480358adbd317d7644f58a9171c2
Captain-Tim <62422680+Captain-Tim@users.noreply.github.com>
2025-09-25 13:11:40 +0800

    Update Backspace String Compare.cpp

--- 0844. Backspace String Compare/Backspace String Compare.cpp
+++ 0844. Backspace String Compare/Backspace String Compare.cpp
@@ -5,36 +5,30 @@ public:
         int t_idx = t.size() - 1;
         while (s_idx >= 0 || t_idx >= 0)
         {
-            moveToNextValidIdx(s, s_idx);
-            moveToNextValidIdx(t, t_idx);
-            if (s_idx >= 0 && t_idx >= 0)
-            {
-                if (s[s_idx] != t[t_idx])
-                    return false;
-                --s_idx;
-                --t_idx;
-            }
-            else if (s_idx >= 0 != t_idx >= 0)
+            char s_char = GetChar(s, s_idx);
+            char t_char = GetChar(t, t_idx);
+            if (s_char != t_char)
                 return false;
+            --s_idx;
+            --t_idx;
         }
         return true;
     }
-    void moveToNextValidIdx(string& str, int& idx)
+private:
+    char GetChar(string& str, int& idx)
     {
         int sharp_num = 0;
         while (idx >= 0)
-        {
-            if (str[idx] == '#')
+        {   
+            if (str[idx] == SHARP)
                 ++sharp_num;
+            else if (sharp_num)
+                --sharp_num;
             else
-            {
-                if (sharp_num)
-                    --sharp_num;
-                else
-                    return;
-            }
+                return str[idx];
             --idx;
         }
-        return;
-    }
+        return ' ';
+    };
+    const char SHARP = '#';
 };
