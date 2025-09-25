6cb80f68ebc0ccbe9cd9323e8cd4c68cbfb02d8a
Captain-Tim <aboutim2020@gmail.com>
2025-09-25 10:52:35 +0800

    Update Valid Parentheses

--- 0020. Valid Parentheses/Valid Parentheses.cpp
+++ 0020. Valid Parentheses/Valid Parentheses.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        unordered_map<char, char> mp;
-        mp[')'] = '(';
-        mp['}'] = '{';
-        mp[']'] = '[';
-        
         stack<char> stk;
         for (char c : s)
         {
-            if (!mp.count(c))
+            if (stk.empty() || mp[c] != stk.top())
                 stk.push(c);
             else
-            {
-                if (stk.empty() || stk.top() != mp[c])
-                    return false;
                 stk.pop();
-            }
         }
         return stk.empty();
     }
+private:
+    unordered_map<char, char> mp{
+        {')', '('},
+        {']', '['},
+        {'}', '{'}
+    };
 };
